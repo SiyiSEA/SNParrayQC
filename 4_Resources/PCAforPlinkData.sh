908a556312e30b4ff23d94274f85695b58c21641
Siyi Wang <s.w.wang@exeter.ac.uk>
2024-06-26 16:46:22 +0100

    PCA function

---
+++ 4_Resources/PCAforPlinkData.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+PCAforPlinkData () {
+    bfile=$1
+    prefix=$2
+    nSD=$3
+
+    ${PLINK}/plink \
+        --bfile ${bfile} \
+        --indep 50 5 1.5 \
+        --out ${RESULTSDIR}/PCAVariants/${prefix}.ld
+
+    ${PLINK}/plink \
+        --bfile ${bfile} \
+        --extract ${RESULTSDIR}/PCAVariants/${prefix}.ld.prune.in \
+        --make-bed --out ${RESULTSDIR}/PCAVariants/${prefix}.ld.prune
+
+    ${GCTA}/gcta-1.94.1 \
+        --bfile ${RESULTSDIR}/PCAVariants/${prefix}.ld.prune \
+        --make-grm-bin \
+        --autosome \
+        --out ${RESULTSDIR}/PCAVariants/${prefix}.imqc
+
+    ${GCTA}/gcta-1.94.1 \
+        --grm ${RESULTSDIR}/PCAVariants/${prefix}.imqc \
+        --pca \
+        --out ${RESULTSDIR}/PCAVariants/${prefix}.imqc.pca
+
+    # plot PCs to identify outliers
+    Rscript ${RESOURCEDIR}/plotPCs.r \
+        ${RESULTSDIR}/PCAVariants/${prefix}.imqc.pca.eigenvec \
+        $nSD
+
+    nOutlierPC1=$(grep -c "\<PC1\>" ${RESULTSDIR}/PCAVariants/${prefix}_OutliersFromPC_${nSD}SDfromMean.txt)
+    echo "There are" ${nOutlierPC1} "outliers for " ${prefix} " on PC1."
+    nOutlierPC2=$(grep -c "\<PC2\>" ${RESULTSDIR}/PCAVariants/${prefix}_OutliersFromPC_${nSD}SDfromMean.txt)
+    echo "There are" ${nOutlierPC2} "outliers for " ${prefix} " on PC2."
+
+    rm ${RESULTSDIR}/PCAVariants/${prefix}.ld* ${RESULTSDIR}/PCAVariants/${prefix}.imqc*
+    rm ${RESULTSDIR}/PCAVariants/${prefix}.ld.prune.b* ${RESULTSDIR}/PCAVariants/${prefix}.ld.prune.fam
+
+}
