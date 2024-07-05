3869a5fa2e80059428e4202284278b524e2204d0
Siyi Wang <s.w.wang@exeter.ac.uk>
2024-07-05 13:49:59 +0100

    comapre on 1000G

---
+++ 6_DarftScripts/JOB03.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#SBATCH --export=ALL # export all environment variables to the batch job.
+#SBATCH -p mrcq # submit to the serial queue
+#SBATCH --time=24:00:00 # Maximum wall time for the job.
+#SBATCH -A Research_Project-MRC190311 # research project to submit under. 
+#SBATCH --nodes=1 # specify number of nodes.
+#SBATCH --mem=100G
+#SBATCH --ntasks-per-node=16 # specify number of processors per node
+#SBATCH --mail-type=END # send email at job completion 
+#SBATCH --output=/lustre/home/sww208/QC/SNParrayQC/5_JobReports/job03liftOverhg38.o
+#SBATCH --error=/lustre/home/sww208/QC/SNParrayQC/5_JobReports/job03liftOverhg38.e
+#SBATCH --job-name=JOB03
+
+source ./config
+source ${RESOURCEDIR}/PCAforPlinkData.sh
+mkdir -p ${PROCESSDIR}/CheckEthnicity/1000GCompare
+cd ${PROCESSDIR}/CheckEthnicity/1000GCompare || exit
+
+
+Ref1000G="/lustre/home/sww208/QC/SNParrayQC/4_Resources/1000G/1000GfromLiftOverhg38/hg19_1000G_from_hg38_updatename"
+
+echo "Update the vairants ID for QCd data---------------------------------------------------------------------------"
+${PLINK2} --bfile ${RESULTSDIR}/02/${FILEPREFIX}_QCd_hg19 \
+                --set-all-var-ids @:#_\$1_\$2 \
+                --make-bed \
+                --out ${FILEPREFIX}_QCd_1kgIDs
+
+
+echo "Frist merge with 1000G in hg19---------------------------------------------------------------------------"
+# first merge with 1000 genomes and filter variants to those in common
+# need to test initially in case of error with triallelic variants
+${PLINK}/plink --bfile ${FILEPREFIX}_QCd_1kgIDs \
+                --bmerge ${Ref1000G} \
+                --make-bed \
+                --out mergedw1000G
+
+echo "Second merge with 1000G in hg38---------------------------------------------------------------------------"
+## issue with variants at same position but different alleles - exclude these
+${PLINK}/plink --bfile ${FILEPREFIX}_QCd_1kgIDs \
+                --exclude mergedw1000G-merge.missnp \
+                --make-bed \
+                --out ${FILEPREFIX}_1kgIDs_forMerge
+
+${PLINK}/plink --bfile ${FILEPREFIX}_1kgIDs_forMerge \
+                --bmerge ${Ref1000G} \
+                --maf 0.01 \
+                --geno 0.1 \
+                --make-bed \
+                --out merged_1000G_fromGH38
+
+# # remove the high LD region (optional)
+# ${PLINK}/plink --bfile ${FILEPREFIX}_merged_1000G_forLD \
+#                 --exclude range ${HighLD} \
+#                 --make-bed \
+#                 --out ${FILEPREFIX}_mergedw1000G
+
+# PCA - 2mins
+echo "PCA the merged data---------------------------------------------------------------------------"
+PCAforPlinkData merged_1000G_fromGH38 merged_1000G_fromGH38 3
+
+# plot PCs - take 11mins
+echo "Plot the PCs---------------------------------------------------------------------------"
+Rscript ${SCRIPTDIR}/4_Resources/plotEthnicity.r ${PROCESSDIR}/CheckEthnicity/1000GCompare ${RESULTSDIR}/PCAVariants/merged_1000G_fromGH38 ${KGG} 
