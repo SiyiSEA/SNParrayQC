38e053a7fbafd977e3b46b28a63e141f614cbf84
Siyi Wang <94650709+SiyiSEA@users.noreply.github.com>
2024-11-12 12:00:35 +0000

    Update 06b_PostQCSanger.sh

--- 06b_PostQCSanger.sh
+++ 06b_PostQCSanger.sh
@@ -225,8 +225,3 @@ cp data_filtered_Sanger* ${RESULTSDIR}/06b/
 # check whether the number of variants is the same
 echo "The number of variants in bim file is" $(wc -l data_filtered_Sanger.bim)
 echo "The number of variants in info file is" $(wc -l data_filtered_Sanger.info)
-
-${PLINK}/plink  --bfile data_filtered_Sanger_test_sex \
-				--make-bed \
-				--out data_filtered_Sanger_test_sex_b37 \
-				--split-x b37 no-fail
